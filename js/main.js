8c3fb95d22733b8a515b028e02a01f81452ca6d5
itera-test <99404324+itera-test@users.noreply.github.com>
2022-02-10 15:21:57 +0300

    new itera

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function copyToClipboard() {
+   const str = document.getElementById('item-to-copy').innerText;
+   const el = document.createElement('textarea');
+   el.value = str;
+   el.setAttribute('readonly', '');
+   el.style.position = 'absolute';
+   el.style.left = '-9999px';
+   document.body.appendChild(el);
+   el.select();
+   document.execCommand('copy');
+   document.body.removeChild(el);
+   alert(`Скопировано: ${str}`);
+}
